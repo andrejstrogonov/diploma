e1f87ce388c76ccaf0e71b3cd4d9fdaf0ab132bc
andrejstrogonov <strogonovandrej@hotmail.com>
2025-09-05 20:53:06 +0300

    Техзадание на исправление ошибок проектирования

--- src/main/java/ru/skypro/homework/HomeworkApplication.java
+++ src/main/java/ru/skypro/homework/HomeworkApplication.java
@@ -1,7 +1,6 @@
 package ru.skypro.homework;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
